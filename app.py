5cca52ce91b5b7d8154769478348edba1a59145c
Breno Almeida <168449309+brenorial@users.noreply.github.com>
2024-07-20 13:29:33 -0300

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os.path
-
 import customtkinter
 from tkinter.filedialog import askdirectory
 from videodownload import baixar_video, baixar_audio
@@ -19,7 +18,6 @@ img = customtkinter.CTkImage(Image.open(img_path), size=(430, 510))
 img_label = customtkinter.CTkLabel(window, image=img, text='')
 img_label.place(x=-5, y=-160)
 
-
 def baixarmp3():
     msg_lbl.configure(text="")
     link = str(link_entry.get())
@@ -27,13 +25,14 @@ def baixarmp3():
         msg_lbl.configure(text="Por favor insira um link.")
         return
     destino = askdirectory()
+    if not destino:
+        msg_lbl.configure(text="Por favor selecione um diretório.")
+        return
     try:
-        baixar_audio(link, destino)
-        msg_lbl.configure(text="Download realizado com sucesso!")
-
+        msg = baixar_audio(link, destino)
+        msg_lbl.configure(text=msg)
     except Exception as e:
         msg_lbl.configure(text=str(e))
-
 
 def baixarmp4():
     msg_lbl.configure(text="")
@@ -42,13 +41,14 @@ def baixarmp4():
         msg_lbl.configure(text="Por favor insira um link.")
         return
     destino = askdirectory()
+    if not destino:
+        msg_lbl.configure(text="Por favor selecione um diretório.")
+        return
     try:
-        baixar_video(link, destino)
-        msg_lbl.configure(text="Download realizado com sucesso!")
-
+        msg = baixar_video(link, destino)
+        msg_lbl.configure(text=msg)
     except Exception as e:
         msg_lbl.configure(text=str(e))
-
 
 def baixarambos():
     msg_lbl.configure(text="")
@@ -57,15 +57,15 @@ def baixarambos():
         msg_lbl.configure(text="Por favor insira um link.")
         return
     destino = askdirectory()
+    if not destino:
+        msg_lbl.configure(text="Por favor selecione um diretório.")
+        return
     try:
-        baixar_audio(link, destino)
-        baixar_video(link, destino)
-        msg_lbl.configure(text="Download realizado com sucesso!")
-
+        msg_audio = baixar_audio(link, destino)
+        msg_video = baixar_video(link, destino)
+        msg_lbl.configure(text=f"{msg_audio}\n{msg_video}")
     except Exception as e:
         msg_lbl.configure(text=str(e))
-
-
 
 link_entry = customtkinter.CTkEntry(window, placeholder_text='URL do Vídeo', width=355, height=28)
 link_entry.place(x=170, y=15)
@@ -82,9 +82,9 @@ mp4_btn.place(x=365, y=55)
 mp3_btn = customtkinter.CTkButton(window, text='Baixar em MP3', width=160, height=28, command=baixarmp3)
 mp3_btn.place(x=170, y=55)
 
-fonte = customtkinter.CTkFont(family='Helvetica',  size=10)
+fonte = customtkinter.CTkFont(family='Helvetica', size=10)
 
-texto = customtkinter.CTkLabel(window,  text="Desenvolvido por Rial", font=fonte)
+texto = customtkinter.CTkLabel(window, text="Desenvolvido por Rial", font=fonte)
 texto.place(x=300, y=210)
 
 window.mainloop()
